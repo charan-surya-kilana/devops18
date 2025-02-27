f4fac8d6b1c4244dc7676c36f553d4396e6fb55c
charan-surya-kilana <awscharan6389@gmail.com>
2025-02-26 18:46:40 -0600

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -22,4 +22,10 @@ versioning_configuration {
 status = "Enabled"
 }
 }
-
+terraform {
+backend "s3" {
+region = "us-east-1"
+bucket = "my.charan.kilana.project"
+key = "prod/terraform.tfstate"
+}
+}
